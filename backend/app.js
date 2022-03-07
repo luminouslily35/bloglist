8ff8a909bce548241615fd7e270caaa44c236577
luminouslily35 <97817095+luminouslily35@users.noreply.github.com>
2022-03-07 15:04:03 -0800

    update frontend

---
+++ backend/app.js
@@ -0,0 +1,35 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+// const Note = require('./models/note')
+const notesRouter = require('./controllers/notes')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+
+app.use('/api/notes', notesRouter)
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
+
+
+app.use(express.json())
+app.use(cors())
+app.use(express.static('build'))
+
+app.use(middleware.requestLogger)
+
+app.use('/api/notes', notesRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
